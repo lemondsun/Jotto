81f15686b42517fd6ed348320e8d3b9dad57774e
Jason Mullings <wjasonmullings@gmail.com>
2020-03-31 21:05:55 -0400

    Start test for GuessedWords

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,4 @@
-import checkPropTypes from 'check-prop-types'
-
+import checkPropTypes from "check-prop-types";
 
 /**
  * Return node(s) with the given data-test attribute.
@@ -8,15 +7,15 @@ import checkPropTypes from 'check-prop-types'
  * @returns {ShallowWrapper}
  */
 export const findByTestAttr = (wrapper, val) => {
-  return wrapper.find(`[data-test="${val}"]`)
-}
+  return wrapper.find(`[data-test="${val}"]`);
+};
 
 export const checkProps = (component, conformingProps) => {
   const propError = checkPropTypes(
-    component.propTypes,
+    component.propsTypes,
     conformingProps,
-    'prop',
+    "prop",
     component.name
-  )
-  expect(propError).toBeUndefined()
-}
+  );
+  expect(propError).toBeUndefined();
+};
